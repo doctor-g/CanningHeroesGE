9219264a7a5dc43aaacb3f9024d60929c65b52a7
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-03-30 18:11:55 -0400

    Add multiple cutting boards

--- project/src/Game.gd
+++ project/src/Game.gd
@@ -1,19 +1,21 @@
 extends Node2D
 
-
 var Carrot = preload("res://src/Food/Carrot/CleanableCarrot.tscn")
 
+onready var _cutting_boards := []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_add_carrot()
+	_cutting_boards = $CuttingBoards.get_children()
+	for item in _cutting_boards:
+		_add_carrot_to(item)
 	
 	
-func _add_carrot():
+func _add_carrot_to(board):
 	var carrot = Carrot.instance()
-	$CuttingBoard.add(carrot)
-	carrot.connect("cleaned", self, "_on_carrot_cleaned", [carrot])
+	board.add(carrot)
+	carrot.connect("cleaned", self, "_on_carrot_cleaned", [board])
 
 
-func _on_carrot_cleaned(carrot):
-	_add_carrot()
+func _on_carrot_cleaned(board):
+	_add_carrot_to(board)
