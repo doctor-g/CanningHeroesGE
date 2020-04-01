6986b98fe4f8911a1ab8e7a2e6c0bd806cb108a9
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-04-01 15:39:31 -0400

    Add fullscreen toggle

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 ## Third-Party Assets
 
 - [Slackey font](https://fonts.google.com/specimen/Slackey) used under [Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+- [Maximize window icon](http://icon-library.com/icon/maximize-window-icon-13.html) used under CC0
--- project/src/UI/MainMenu.gd
+++ project/src/UI/MainMenu.gd
@@ -2,5 +2,11 @@ extends Sprite
 
 signal game_selected(selection)
 
+onready var _fullscreen_toggle = $Interface/TextureRect/FullscreenToggle
+
 func _on_CarrotButton_pressed():
 	emit_signal("game_selected", "carrot")
+
+
+func _on_FullscreenToggle_pressed():
+	OS.window_fullscreen = _fullscreen_toggle.pressed
